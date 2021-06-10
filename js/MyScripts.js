2f711806618263ccb56709c5f87f7c06bd660f9a
PGO-Alex <oguevara.alex@gmail.com>
2021-06-10 10:53:19 -0500

    mobile view fixed
    -added some effects
    -fixed mobile view
    -add some interaction styles

--- js/MyScripts.js
+++ js/MyScripts.js
@@ -1,18 +1,15 @@
 function openTab(eve,option) {
 
-    // Get all elements with class="tabcontent" and hide them
     var tabcontent = document.getElementsByClassName("tab-content");
     for (var i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
     }
   
-    // Get all elements with class="tablinks" and remove the class "active"
     var tablinks = document.getElementsByClassName("tab-link");
     for (var i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
+      tablinks[i].className = tablinks[i].className.replace(" active ", "");
     }
   
-    // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(option).style.display = "block";
     eve.currentTarget.className += " active ";
 }
